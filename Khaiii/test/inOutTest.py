91fdc5659bab834fab68914da561762798728101
mr-laughaway <mr.laughaway@gmail.com>
2019-10-02 17:46:57 +0900

    Khaiii Test code push

---
+++ Khaiii/test/inOutTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# inOutTest.py
+
+import sys
+
+print('sys.argv 길이 : ', len(sys.argv))                                             
+ 
+ 
+for arg in sys.argv: 
+    print('arg value = ', arg) 
