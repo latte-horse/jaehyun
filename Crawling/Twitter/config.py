cd817b7be0b7f34bea9be0bd544165b13ace65bc
Jaehyun <50945802+Mr-Laughaway@users.noreply.github.com>
2019-10-04 17:13:03 +0900

    Create config.py
    dummy file for 'config.py'

---
+++ Crawling/Twitter/config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generated via Twitter's Application Management:
+#	https://developer.twitter.com/en/apps
+#--------------------------------------------------------------------------------
+
+consumer_key = "X"
+consumer_secret = "X"
+access_key = "X"
+access_secret = "X"
