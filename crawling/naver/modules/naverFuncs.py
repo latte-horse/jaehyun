f28d5110efc56d02c9fc04c2a9b2255035706779
codeorchord <codeorchord@gmail.com>
2019-10-06 16:33:29 +0900

    NAVER 인기 검색어 및 관련 뉴스 추출코드

---
+++ crawling/naver/modules/naverFuncs.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#naverFuncs.py
+
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+import json
+if __name__ != '__main__':
+    from . import config
+
+
+#--------------------------------------------------------------------------
+# 실시간 인기 검색어 cnt개 반환
+#--------------------------------------------------------------------------
+def getKeywords(cnt):
+    naverKeywords = []
+    url_naver = "http://www.naver.com"
+
+    html = requests.get(url_naver).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    listHtml = soup.select('.ah_roll_area .ah_k')
+
+    #naverKeywords 변수에 keyword 추가
+    for keyword in listHtml:
+        naverKeywords.append(keyword.get_text())
+
+    #cnt 개의 결과만을 반환
+    return naverKeywords[:cnt]
+
+
+#--------------------------------------------------------------------------
+# 검색어로 뉴스를 검색하여 cnt개 반환
+#--------------------------------------------------------------------------
+def getNewsList(search_words, cnt):
+    encText = urllib.parse.quote(search_words)
+    url = "https://openapi.naver.com/v1/search/news.json?query={0}&display={1}&sort={2}".format(
+        encText, cnt, "date"
+    ) 
+
+    #naver API를 이용하여 검색
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", config.clientID)
+    request.add_header("X-Naver-Client-Secret", config.clientSecret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        newsList = json.loads(response_body.decode('utf-8'))['items']
+
+        #title과 link만 추출하여 담기
+        resultList = []
+        for news in newsList:
+            resultList.append({ 
+                'title' : re.sub("<[^>]*>", '', news['title']),
+                'link' : news['link']})
+        
+        #어떤 검색어로 검색한 결과인지를 포함하여 반환
+        return resultList
+
+    else:
+        print("Error Code:" + rescode)
+        exit(1)
+
+
+#--------------------------------------------------------------------------
+# module test code
+#--------------------------------------------------------------------------
+if __name__ == "__main__":
+    naverKeywords = getKeywords(10)
+    print(naverKeywords)
+    import config
+    print(getNewsList(naverKeywords[0], 10))
---
+++ crawling/naver/naver_crawling.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#naver_crawling.py
+
+from modules import naverFuncs
+import json
+import unicodedata
+
+#--------------------------------------------------------------------------
+# 실시간 인기 검색어 10개 추출
+#--------------------------------------------------------------------------
+naver_keywords = naverFuncs.getKeywords(10)
+#for keyword in naverKeywords: print(keyword)
+
+#--------------------------------------------------------------------------
+# 검색어당 30개씩의 뉴스 제목과 링크 추출
+#--------------------------------------------------------------------------
+naver_list = []
+for search_words in naver_keywords[:2]: #테스트로 2개만
+    news_list = naverFuncs.getNewsList(search_words, 3) #테스트로 3개만
+    naver_list.append({'search_words' : search_words, 'items' : news_list})
+
+#--------------------------------------------------------------------------
+# 결과 확인
+#--------------------------------------------------------------------------
+for elem in naver_list:
+    print("검색어: " + elem['search_words'])
+    for news in elem['items']:
+        print(news)
+    print("")
+
+
+# 추후 json 변환이 필요할 경우 참조 코드
+# dump = json.dumps(target_str, ensure_ascii = False).encode("utf-8")
