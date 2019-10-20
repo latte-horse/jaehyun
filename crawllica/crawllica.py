a60d629c89ffb346347f3499babe909a0f918616
codeorchord <codeorchord@gmail.com>
2019-10-20 22:54:02 +0900

    Revert "[minor]크롤리카 코드-리팩토링"
    This reverts commit 55a012a440949f6512924e3dac7ccb1d44521da2.

--- crawllica/crawllica.py
+++ crawllica/crawllica.py
@@ -1,49 +1,28 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #crawllica.py
 
 from harvesta import harvesta
 from preproca import preproca
 import platform
 import sys
-import subprocess
+
+def doCrawlling(path):
+    return harvesta.harvest(path)
+
+def  doPreproc(path):
+    preproca.preproc(path)
 
 
-#------------------------------------------------------------------------------
-# MAIN driver code
-#------------------------------------------------------------------------------
 if __name__ == "__main__":
-    #-------------------------------
-    # 윈도우에서는 테스트만 돌림
-    #-------------------------------
     if platform.system() == "Windows":
+        outPath = doCrawlling("c:\\crawllica_output")
         # outPath = "C:\\crawllica_output\\191016\\1710"
-        outPath = harvesta.harvest("c:\\crawllica_output")
         doPreproc(outPath)
-
-    
-    #--------------------------------
-    # 실제 리눅스 실행 환경
-    #--------------------------------
     else:
-        # 아규먼트 체크
         if len(sys.argv) < 2:
-            print("usage: crawllica [output-root path]")
+            print("usage: crawllica [output path]")
             exit(1)
         
-        #----------------------------
-        # 수집 / 전처리
-        #----------------------------
-        # harvesta 실행
-        outPath = harvesta.harvest(sys.argv[1])
-        # proproca 실행
-        preproca.preproc(outPath)
-
-        #----------------------------
-        # 분석 -> analyza.py 실행
-        #----------------------------
-        # outPath = "../data/191020/2200"
-        p = subprocess.Popen(["../analyza/analyza.py", outPath])
-        p.wait()
-        print("모든 작업 완료")
-
-        
+        outPath = doCrawlling(sys.argv[1])
+        doPreproc(outPath)
